ccde74679decac6ac0fdd74baefd4e2267d835b5
Artur Tkachenko <1816594+fhnb16@users.noreply.github.com>
2023-03-27 10:35:50 +0500

    make nav component and story compact view on desktop/tablet

--- market-test/src/components/modal_component.vue
+++ market-test/src/components/modal_component.vue
@@ -40,6 +40,48 @@ const store = useStore();
 }
 
 /*////////////////////
+        modal
+////////////////////*/
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.modal-contact .modal-header > h2 {
+  margin: 24px;
+  margin-bottom: 6px;
+  margin-top: 24px;
+}
+
+.modal-close {
+  color: transparent;
+  font-size: 1px;
+  cursor: pointer;
+  position: relative;
+  width: 24px;
+  height: 24px;
+  margin: 24px;
+  background-position: center;
+  margin-top: 24px;
+  border-radius: 50%;
+  background-image: url("data:image/svg+xml,%0A%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M16 28C22.5647 28 28 22.5529 28 16C28 9.43529 22.5529 4 15.9882 4C9.43529 4 4 9.43529 4 16C4 22.5529 9.44706 28 16 28ZM12.0471 20.9412C11.5059 20.9412 11.0824 20.5059 11.0824 19.9647C11.0824 19.7059 11.1765 19.4588 11.3647 19.2824L14.6235 16.0118L11.3647 12.7529C11.1765 12.5647 11.0824 12.3294 11.0824 12.0706C11.0824 11.5176 11.5059 11.1059 12.0471 11.1059C12.3176 11.1059 12.5294 11.2 12.7176 11.3765L16 14.6471L19.3059 11.3647C19.5059 11.1647 19.7176 11.0824 19.9765 11.0824C20.5176 11.0824 20.9529 11.5059 20.9529 12.0471C20.9529 12.3176 20.8706 12.5294 20.6588 12.7412L17.3882 16.0118L20.6471 19.2706C20.8471 19.4471 20.9412 19.6941 20.9412 19.9647C20.9412 20.5059 20.5059 20.9412 19.9529 20.9412C19.6824 20.9412 19.4353 20.8471 19.2588 20.6588L16 17.3882L12.7529 20.6588C12.5647 20.8471 12.3176 20.9412 12.0471 20.9412Z' fill='%23090909' fill-opacity='0.3'/%3E%3C/svg%3E%0A");
+}
+
+.modal-contact .modal-header .modal-close {
+  margin-bottom: 6px;
+}
+
+.modal-close:hover {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+}
+
+.modal-close:active {
+  box-shadow: inset 0 2px 8px rgba(0, 0, 0, 0.3);
+}
+
+/*////////////////////
   media screen width
 ////////////////////*/
 
--- market-test/src/views/MainPage.vue
+++ market-test/src/views/MainPage.vue
@@ -6,6 +6,7 @@ import StoriesWrapper from "../components/stories_wrapper.vue";
 import Button from "../components/button_component.vue";
 import Section from "../components/section_component.vue";
 import ModalContact from "../components/modal_contact_component.vue";
+import NavMenu from "../components/navMenu_component.vue";
 
 const store = useStore();
 /*const getContent = computed(() => {
@@ -22,6 +23,9 @@ const stories = computed(() => {
 });
 const modalVisible = computed(() => {
   return store.state.modalVisible;
+});
+const activeStory = computed(() => {
+  return store.state.activeStory;
 });
 onBeforeMount(() => {
   store.dispatch("fetchContent");
@@ -65,87 +69,9 @@ document.addEventListener(
       />
     </div>
     <span class="break"></span>
-    <nav class="fixed-bottom-menu">
-      <div class="menu-item active" v-on:click="store.dispatch('fetchContent')">
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M20.5062 10.274L13.0062 3.45836C12.868 3.33187 12.6874 3.26172 12.5 3.26172C12.3126 3.26172 12.132 3.33187 11.9937 3.45836L4.49375 10.274C4.41828 10.3453 4.35783 10.4309 4.31597 10.5259C4.27411 10.6209 4.25168 10.7233 4.25 10.8271V19.499C4.25 19.6979 4.32902 19.8887 4.46967 20.0293C4.61032 20.17 4.80109 20.249 5 20.249H20C20.1989 20.249 20.3897 20.17 20.5303 20.0293C20.671 19.8887 20.75 19.6979 20.75 19.499V10.8271C20.7483 10.7233 20.7259 10.6209 20.684 10.5259C20.6422 10.4309 20.5817 10.3453 20.5062 10.274Z"
-            fill="white"
-            stroke="currentColor"
-            stroke-width="2.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        <span>Главная</span>
-      </div>
-      <div class="menu-item">
-        <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M256 160c16-63.16 76.43-95.41 208-96a15.94 15.94 0 0116 16v288a16 16 0 01-16 16c-128 0-177.45 25.81-208 64-30.37-38-80-64-208-64-9.88 0-16-8.05-16-17.93V80a15.94 15.94 0 0116-16c131.57.59 192 32.84 208 96zM256 160v288"
-            fill="white"
-            stroke="currentColor"
-            stroke-width="42"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        <span>Каталог</span>
-      </div>
-      <div class="menu-item">
-        <svg viewBox="0 0 24 22" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M11.9987 0.00195312C10.4968 0.00195312 9.02827 0.445518 7.77749 1.27699C6.57953 2.07335 5.63237 3.19158 5.04377 4.50133C4.2024 4.90806 3.44698 5.47394 2.81961 6.16835C2.14521 6.91481 1.63354 7.79345 1.31714 8.74839C1.00074 9.70333 0.886537 10.7137 0.981796 11.7151C1.07705 12.7166 1.37969 13.6873 1.87045 14.5655C2.36121 15.4436 3.02936 16.21 3.8324 16.816C4.63543 17.4219 5.55578 17.8541 6.53491 18.085C7.51403 18.3159 8.5305 18.3406 9.51967 18.1574C9.94016 18.0795 10.3518 17.9648 10.75 17.8151V20.75C10.75 21.4404 11.3096 22 12 22C12.6904 22 13.25 21.4404 13.25 20.75V17.8161C13.6474 17.9653 14.0582 18.0796 14.4777 18.1574C15.4669 18.3406 16.4834 18.3159 17.4625 18.085C18.4416 17.8541 19.362 17.4219 20.165 16.816C20.968 16.21 21.6362 15.4436 22.127 14.5655C22.6177 13.6873 22.9204 12.7166 23.0156 11.7151C23.1109 10.7137 22.9967 9.70333 22.6803 8.74839C22.3639 7.79345 21.8522 6.91481 21.1778 6.16835C20.5504 5.47394 19.795 4.90806 18.9536 4.50133C18.365 3.19158 17.4179 2.07336 16.2199 1.27699C14.9691 0.445518 13.5006 0.00195312 11.9987 0.00195312ZM13.25 15.0373C13.7631 15.3634 14.3344 15.5883 14.9331 15.6992C15.5811 15.8192 16.2471 15.8031 16.8886 15.6518C17.5301 15.5005 18.1331 15.2173 18.6592 14.8203C19.1853 14.4233 19.6231 13.9212 19.9446 13.3459C20.2662 12.7705 20.4644 12.1345 20.5268 11.4784C20.5893 10.8223 20.5144 10.1603 20.3071 9.53468C20.0998 8.90903 19.7646 8.33337 19.3228 7.84431C18.8809 7.35525 18.3421 6.96349 17.7407 6.69396C17.7237 6.68637 17.707 6.67841 17.6904 6.67007C17.2649 6.45618 16.9269 6.10124 16.734 5.66585L16.7243 5.64332C16.3335 4.7125 15.6766 3.91781 14.8359 3.35895C13.9952 2.80009 13.0082 2.50195 11.9987 2.50195C10.9892 2.50195 10.0022 2.80009 9.16149 3.35895C8.3208 3.91781 7.66388 4.7125 7.27315 5.64332L7.26343 5.66585C7.07053 6.10124 6.7325 6.45618 6.30704 6.67007C6.29045 6.67841 6.27369 6.68637 6.25675 6.69396C5.65529 6.96349 5.11649 7.35525 4.67465 7.84431C4.2328 8.33337 3.89757 8.90903 3.69027 9.53468C3.48297 10.1603 3.40815 10.8223 3.47056 11.4784C3.53297 12.1345 3.73125 12.7705 4.05278 13.3459C4.37432 13.9212 4.81207 14.4233 5.3382 14.8203C5.86432 15.2173 6.46731 15.5005 7.10881 15.6518C7.75031 15.8031 8.41627 15.8192 9.06434 15.6992C9.66404 15.5881 10.2363 15.3627 10.75 15.0356V14.0225L6.94098 12.118C6.32351 11.8093 6.07323 11.0585 6.38197 10.441C6.6907 9.82351 7.44154 9.57323 8.05902 9.88197L10.75 11.2275V11.0271C10.7496 11.0096 10.7496 10.9922 10.75 10.9749V7.25C10.75 6.55964 11.3096 6 12 6C12.6904 6 13.25 6.55964 13.25 7.25V8.97746L15.941 7.63197C16.5585 7.32323 17.3093 7.57351 17.618 8.19098C17.9268 8.80846 17.6765 9.5593 17.059 9.86803L13.25 11.7725V13.2248C13.2504 13.2422 13.2504 13.2597 13.25 13.2771V15.0373Z"
-            fill="currentColor"
-            stroke="white"
-            stroke-width="0.1"
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-          />
-        </svg>
-        <span>Landscape</span>
-      </div>
-      <div class="menu-item">
-        <svg
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          width="24"
-        >
-          <path
-            d="M4.25 12C4.25 7.71979 7.71979 4.25 12 4.25C16.2802 4.25 19.75 7.71979 19.75 12C19.75 16.2802 16.2802 19.75 12 19.75C7.71979 19.75 4.25 16.2802 4.25 12ZM12 1.75C6.33908 1.75 1.75 6.33908 1.75 12C1.75 17.6609 6.33908 22.25 12 22.25C17.6609 22.25 22.25 17.6609 22.25 12C22.25 6.33908 17.6609 1.75 12 1.75ZM8.375 12C8.375 9.99797 9.99797 8.375 12 8.375C14.002 8.375 15.625 9.99797 15.625 12C15.625 14.002 14.002 15.625 12 15.625C9.99797 15.625 8.375 14.002 8.375 12ZM12 5.875C8.61726 5.875 5.875 8.61726 5.875 12C5.875 15.3827 8.61726 18.125 12 18.125C15.3827 18.125 18.125 15.3827 18.125 12C18.125 8.61726 15.3827 5.875 12 5.875Z"
-            fill="currentColor"
-            stroke="white"
-            stroke-width="0.1"
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            shape-rendering="geometricPrecision"
-          />
-        </svg>
-        <span>Стройка</span>
-      </div>
-      <div class="menu-item">
-        <svg
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          width="24"
-        >
-          <path
-            d="M7.25 9C7.25 6.37665 9.37664 4.25 12 4.25C14.6233 4.25 16.75 6.37665 16.75 9C16.75 11.6234 14.6233 13.75 12 13.75C9.37664 13.75 7.25 11.6234 7.25 9ZM16.5321 14.6592C18.1891 13.3305 19.25 11.2892 19.25 9C19.25 4.99594 16.0041 1.75 12 1.75C7.99593 1.75 4.75 4.99594 4.75 9C4.75 11.2892 5.81093 13.3305 7.46792 14.6592C7.00787 14.8515 6.5591 15.0735 6.12453 15.3245C4.33819 16.3559 2.85486 17.8395 1.82366 19.6259C1.47853 20.2238 1.68345 20.9883 2.28134 21.3334C2.87924 21.6785 3.64371 21.4736 3.98883 20.8757C4.80063 19.4694 5.96836 18.3015 7.37463 17.4895C8.78089 16.6775 10.3761 16.25 12 16.25C13.6239 16.25 15.2191 16.6775 16.6254 17.4895C18.0316 18.3015 19.1994 19.4694 20.0112 20.8757C20.3563 21.4736 21.1207 21.6785 21.7186 21.3334C22.3165 20.9883 22.5215 20.2238 22.1763 19.6259C21.1451 17.8395 19.6618 16.3559 17.8755 15.3245C17.4409 15.0735 16.9921 14.8515 16.5321 14.6592Z"
-            fill="currentColor"
-            stroke="white"
-            stroke-width="0.1"
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            shape-rendering="geometricPrecision"
-          />
-        </svg>
-        <span>Меню</span>
-      </div>
-    </nav>
+    <NavMenu />
     <ModalContact />
-    <div class="background-blur" v-if="modalVisible"></div>
+    <div class="background-blur" v-if="modalVisible || activeStory != -1"></div>
   </div>
 </template>
 
@@ -222,114 +148,6 @@ span.break:before {
 }
 
 /*////////////////////
-        modal
-////////////////////*/
-
-.modal-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.modal-contact .modal-header > h2 {
-  margin: 24px;
-  margin-bottom: 6px;
-  margin-top: 24px;
-}
-
-.modal-close {
-  color: transparent;
-  font-size: 1px;
-  cursor: pointer;
-  position: relative;
-  width: 24px;
-  height: 24px;
-  margin: 24px;
-  background-position: center;
-  margin-top: 24px;
-  border-radius: 50%;
-  background-image: url("data:image/svg+xml,%0A%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M16 28C22.5647 28 28 22.5529 28 16C28 9.43529 22.5529 4 15.9882 4C9.43529 4 4 9.43529 4 16C4 22.5529 9.44706 28 16 28ZM12.0471 20.9412C11.5059 20.9412 11.0824 20.5059 11.0824 19.9647C11.0824 19.7059 11.1765 19.4588 11.3647 19.2824L14.6235 16.0118L11.3647 12.7529C11.1765 12.5647 11.0824 12.3294 11.0824 12.0706C11.0824 11.5176 11.5059 11.1059 12.0471 11.1059C12.3176 11.1059 12.5294 11.2 12.7176 11.3765L16 14.6471L19.3059 11.3647C19.5059 11.1647 19.7176 11.0824 19.9765 11.0824C20.5176 11.0824 20.9529 11.5059 20.9529 12.0471C20.9529 12.3176 20.8706 12.5294 20.6588 12.7412L17.3882 16.0118L20.6471 19.2706C20.8471 19.4471 20.9412 19.6941 20.9412 19.9647C20.9412 20.5059 20.5059 20.9412 19.9529 20.9412C19.6824 20.9412 19.4353 20.8471 19.2588 20.6588L16 17.3882L12.7529 20.6588C12.5647 20.8471 12.3176 20.9412 12.0471 20.9412Z' fill='%23090909' fill-opacity='0.3'/%3E%3C/svg%3E%0A");
-}
-
-.modal-contact .modal-header .modal-close {
-  margin-bottom: 6px;
-}
-
-.modal-close:hover {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
-}
-
-.modal-close:active {
-  box-shadow: inset 0 2px 8px rgba(0, 0, 0, 0.3);
-}
-
-/*////////////////////
-        nav
-////////////////////*/
-
-nav.fixed-bottom-menu {
-  background-color: var(--color-gray-light);
-  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.08), 0px 2px 24px rgba(0, 0, 0, 0.08);
-  border-radius: 18px 18px 0px 0px;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  padding-bottom: 6px;
-  height: 56px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  align-items: stretch;
-  z-index: 6;
-}
-
-nav.fixed-bottom-menu > .menu-item {
-  font-family: var(--text-font-rich), sans-serif;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 8px;
-  line-height: 150%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  flex-wrap: nowrap;
-  width: 20%;
-}
-
-nav.fixed-bottom-menu > .menu-item,
-nav.fixed-bottom-menu > .menu-item * {
-  cursor: pointer;
-  transition: var(--transition-default);
-}
-
-nav.fixed-bottom-menu > .menu-item:hover {
-  transform: translateY(-2px);
-}
-
-nav.fixed-bottom-menu > .menu-item:active {
-  transform: translateY(-1px);
-}
-
-nav.fixed-bottom-menu > .menu-item svg {
-  height: 24px;
-  width: 24px;
-}
-
-nav.fixed-bottom-menu > .menu-item,
-nav.fixed-bottom-menu > .menu-item * {
-  color: var(--color-black);
-}
-
-nav.fixed-bottom-menu > .menu-item.active,
-nav.fixed-bottom-menu > .menu-item.active *,
-nav.fixed-bottom-menu > .menu-item:active,
-nav.fixed-bottom-menu > .menu-item:active * {
-  color: var(--color-blue);
-}
-
-/*////////////////////
   media screen width
 ////////////////////*/
 
@@ -342,9 +160,6 @@ nav.fixed-bottom-menu > .menu-item:active * {
     transition: var(--transition-default);
     text-align: center;
     font-size: 32px;
-  }
-  nav.fixed-bottom-menu {
-    justify-content: center;
   }
 }
 </style>
